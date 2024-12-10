df6d9e3e9bfc25ac81e5d96ce49610701327f705
Dan Nguyen <Eo-Le@Hotmail.com>
2024-12-10 12:47:23 +0000

    try to apply javascript to jekyll import and export doesn't work fix around is to return the array object from a function

--- JS/Persons.js
+++
@@ -1,8 +0,0 @@
-// source.js
-const myObject = {
-    name: "John Doe",
-    age: 30,
-    profession: "Developer"
-};
-
-module.exports = myObject;
---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+function sayHello() {
+    alert('Hej fra assets/js/script.js');
+}
