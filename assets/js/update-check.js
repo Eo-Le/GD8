e170b48bbd87d7f5111f8e6eb705f8dc66a4c8e0
Dan Nguyen <Eo-Le@Hotmail.com>
2024-12-13 21:24:24 +0000

    try to understand the update content commenting out some meta tags

--- assets/js/update-check.js
+++ assets/js/update-check.js
@@ -1,15 +1,15 @@
 // Fetch the current update timestamp from the meta tag
 const currentUpdate = document.querySelector('meta[name="last-update"]').getAttribute('content');
 
-// Retrieve the previous update timestamp from localStorage
+// Retrieve the previous update timestamp from localStorage. If no key 'lastUpdate' is found then the value will be null
 const previousUpdate = localStorage.getItem('lastUpdate');
 
 // Compare timestamps to detect updates
 if (previousUpdate && previousUpdate !== currentUpdate) {
-    alert(`Hjemmesiden er opdateret.\nGamle opdatering: ${previousUpdate}\nNy opdatering: ${currentUpdate}`);
+    alert(`Hjemmesiden er opdateret siden sidst\nGamle opdatering, dato: ${previousUpdate}\nNy opdatering, dato: ${currentUpdate}`);
 } else {
-    // alert(`No updates.\npreviousUpdate: ${previousUpdate}\ncurrentUpdate: ${currentUpdate}`);
+    // do nothing
 }
 
-// Store the latest timestamp in localStorage
+// Store the latest timestamp in localStorage. https://www.w3schools.com/js/js_api_web_storage.asp
 localStorage.setItem('lastUpdate', currentUpdate);
