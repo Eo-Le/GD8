40f740b9b095f0b2292611e15972dc1a7599c1bb
Dan Nguyen <Eo-Le@Hotmail.com>
2024-12-17 18:46:19 +0000

    fix Sandra's dob add alert add some bg colour

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,1 @@
-console.log("loaded: assets/js/script.js");
 
-document.addEventListener("DOMContentLoaded", function () {
-    document.querySelector('button').addEventListener('click', function () {
-        alert('Alert fra assets/js/script.js.');
-    });
-});
